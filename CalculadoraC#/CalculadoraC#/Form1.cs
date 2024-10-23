9368d9efa4d67ee97b9b069647f978830b2ee879
Lucas Castro <169384752+LuquinhasCTR@users.noreply.github.com>
2024-10-23 11:56:14 +0100

    Ajuste de erros

--- CalculadoraC#/CalculadoraC#/Form1.cs
+++ CalculadoraC#/CalculadoraC#/Form1.cs
@@ -14,6 +14,7 @@ namespace CalculadoraC_
 {
     public partial class Form1 : Form
     {
+        public bool operacaoAtivada = false;
         public int contar = 1;
         /// <summary>
         /// Armazena o tipo de operação
@@ -50,6 +51,28 @@ namespace CalculadoraC_
             valor1 = Double.Parse(textBox1.Text);
             operacao = op;
             operacaoPressionada = true;
+        }
+        private void ApagarRaiz()
+        {
+            if (operacaoAtivada==true)
+            {
+                textBox1.Text = "0";
+                valor1 = 0;
+                valor2 = 0;
+                AdicionarNumero("0");
+                operacaoAtivada = false;
+            }
+        }
+        private void ApagarPotencia()
+        {
+            if (operacaoAtivada == true)
+            {
+                textBox1.Text = "0";
+                valor1 = 0;
+                valor2 = 0;
+                AdicionarNumero("0");
+                operacaoAtivada = false;
+            }
         }
         public Form1()
         {
@@ -92,114 +115,64 @@ namespace CalculadoraC_
         }
         private void BotaoZero_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("0");
         }
         private void BotaoUm_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();   
+            ApagarRaiz();
             AdicionarNumero("1");
         }
         private void BotaoDois_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("2");
         }
         private void BotaoTres_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("3");
         }
         private void BotaoQuatro_Click(object sender, EventArgs e)
         {
-            if (contar >=2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("4");
         }
         private void BotaoCinco_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("5");
         }
         private void BotaoSeis_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("6");
         }
         private void BotaoSete_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("7");
         }
 
         private void BotaoOito_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("8");
         }
 
         private void BotaoNove_Click(object sender, EventArgs e)
         {
-            if (contar >= 2)
-            {
-                textBox1.Text = "0";
-                valor1 = 0;
-                valor2 = 0;
-                AdicionarNumero("0");
-            }
+            ApagarPotencia();
+            ApagarRaiz();
             AdicionarNumero("9");
         }
         private void BotaoVirgula_Click(object sender, EventArgs e)
@@ -245,8 +218,9 @@ namespace CalculadoraC_
                     else
                         textBox1.Text = "Erro: Div/0";
                     break;
-                case '^':   
-                        textBox1.Text = Math.Pow(valor1, valor2).ToString(); // Calculo de potência                    
+                case '^':
+                    textBox1.Text = Math.Pow(valor1, valor2).ToString(); // Calculo de potência                    
+                    operacaoAtivada = true;
                     break;
                 case '%': textBox1.Text=((valor1/100)*valor2).ToString();
                     break;
@@ -261,14 +235,17 @@ namespace CalculadoraC_
         }
         private void BotaoFracao_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Código não implementado para este botão", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            this.ShowDialog();
+            //MessageBox.Show("Código não implementado para este botão", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //this.ShowDialog();
+            valor1 = double.Parse(textBox1.Text);
+            textBox1.Text = (1 / valor1).ToString();
         }
         private void BotaoPotencia_Click(object sender, EventArgs e)
         {
             //MessageBox.Show("Código não implementado para este botão", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
             //this.ShowDialog();
             SetOperacao('^');
+            
             
         }
         private void BotaoRaiz_Click(object sender, EventArgs e)
@@ -282,6 +259,7 @@ namespace CalculadoraC_
             {
                 textBox1.Text = "Erro: Raiz Negativa";
             }
+            operacaoAtivada = true;
         }
         private void BotaoCE_Click(object sender, EventArgs e)
         {
